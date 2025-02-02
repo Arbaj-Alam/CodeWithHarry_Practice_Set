894a91542815b187cdffa320cbbfce00df30f1c1
Arbaj-Alam <arbajapp.dev@gmail.com>
2025-02-02 14:05:47 +0530

    Practice set 02 is Completed.

--- src/CWH_Practice/CWH_Practice_Set_02.java
+++ src/CWH_Practice/CWH_Practice_Set_02.java
@@ -1,4 +1,35 @@
 package CWH_Practice;
 
 public class CWH_Practice_Set_02 {
+
+    public static void main(String[] args) {
+
+        // Question no.01
+        // What will be the result of the following expression float a = 7/4*9/2
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        // Question no.02
+        // Write a java program to encrypt a grade by adding 8 to it.Decrypt it to show the correct grade.
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        // Question no.03
+        // Use Comparison operator to find otu whether a given number is greater than the user entered number or not.
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        // Question no.04
+        // Write the following expression in a java program.((v*v) - (u*u)) / 2*a*s
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        // Question no.05
+        // Find the value of the following expression:
+        // int x = 7;
+        // int a = 7.49 / 7 + 35/7
+        // Value of a =?
+
+
+    }
 }
